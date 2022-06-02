c5551c51c3ad52706d44ebc8548a789956850319
Akalanka Perera <akalankaperera128@gmail.com>
2022-06-02 11:51:45 +0530

    fix: commit message

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ run({command: 'git', args: [`show`, `./`]}).then((diff) => {
         versionUpdate = 'minor';
       }
       run({command: 'npm', args: [`--no-git-tag-version`, `version`, versionUpdate]}).then(() => {
-        const successMsg = `Bumped version of ${name} to match latest ${versionUpdate} release`
+        const successMsg = `bumped version of ${name} to match latest ${versionUpdate} release`
         run({command: 'git', args: [`add`, '.']}).then(() => {
           run({command: 'git', args: [`commit`, '-m', `${successMsg}`]}).then(() => {
             console.log(successMsg);
