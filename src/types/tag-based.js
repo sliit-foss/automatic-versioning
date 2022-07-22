22c7ec33eee887cf67a81065fdc12bc2954b8107
Akalanka <akalankaperera128@gmail.com>
2022-07-22 23:44:06 +0530

    Fix: tag comparison

--- src/types/tag-based.js
+++ src/types/tag-based.js
@@ -1,13 +1,14 @@
 const run = require('../utils/runner');
 
 module.exports = (name) => {
-    run({ command: 'npm', args: [`pkg`, 'get', 'version'] }).then((initalVersion) => {
-        initalVersion = initalVersion.replace(/\n/g, '')?.replaceAll('\"', '')?.replaceAll('-', '')?.trim();
+    run({ command: 'npm', args: [`pkg`, 'get', 'version'] }).then((initialVersion) => {
+        initialVersion = initialVersion.replace(/\n/g, '')?.replaceAll('\"', '')?.trim();
+        if (/[0-9]{4}.[0-9]{2}.[0-9]{2}.rc/.test(initialVersion)) initialVersion = initialVersion?.replaceFirst('-', '')
         run({ command: 'git', args: [`tag`, '--sort=committerdate'] }).then((tags) => {
-            const latest = tags.split('\n')?.reverse()[1]?.trim()?.replace('v', '')?.replace('_', '').replace('-', '');
-            if (latest && latest !== initalVersion) {
+            const latest = tags.split('\n')?.reverse()[1]?.trim()?.replace('v', '')?.replace('_', '-');
+            if (latest && latest !== initialVersion) {
                 run({ command: 'npm', args: [`version`, latest, '--allow-same-version', '--no-git-tag-version'] }).then(() => {
-                    const successMsg = `Bumped version of ${name} from ${initalVersion} to ${latest}`
+                    const successMsg = `Bumped version of ${name} from ${initialVersion} to ${latest}`
                     run({ command: 'git', args: [`add`, '.'] }).then(() => {
                         run({ command: 'git', args: [`commit`, '-m', `${successMsg}`] }).then(() => {
                             run({ command: 'git', args: [`status`] }).then((status) => {
